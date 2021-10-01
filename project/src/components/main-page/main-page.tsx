d6a941887ed878ce243e0ab7ff5f93e054cf2e13
sylvan008 <sylvan.climb@gmail.com>
2021-10-01 14:12:48 +0300

    Добавит компонент Footer

--- project/src/components/main-page/main-page.tsx
+++ project/src/components/main-page/main-page.tsx
@@ -1,4 +1,5 @@
 import FilmCard from '../film-card/film-card';
+import Footer from '../footer/footer';
 
 type Promo = {
   name: string,
@@ -140,19 +141,7 @@ function MainPage(props: PropsType): JSX.Element {
           </div>
         </section>
 
-        <footer className="page-footer">
-          <div className="logo">
-            <a className="logo__link logo__link--light">
-              <span className="logo__letter logo__letter--1">W</span>
-              <span className="logo__letter logo__letter--2">T</span>
-              <span className="logo__letter logo__letter--3">W</span>
-            </a>
-          </div>
-
-          <div className="copyright">
-            <p>© 2019 What to watch Ltd.</p>
-          </div>
-        </footer>
+        <Footer />
       </div>
     </>
   );
