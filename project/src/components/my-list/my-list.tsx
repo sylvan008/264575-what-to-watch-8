3c753bab68bea92a2b389dad472605c2fe829590
sylvan008 <sylvan.climb@gmail.com>
2021-10-01 14:35:15 +0300

    Добавит компонент MyList

---
+++ project/src/components/my-list/my-list.tsx
@@ -0,0 +1,46 @@
+import Logo from '../logo/logo';
+import Footer from '../footer/footer';
+import FilmCard from '../film-card/film-card';
+
+function MyList(): JSX.Element {
+  return (
+    <div className="user-page">
+      <header className="page-header user-page__head">
+        <Logo />
+
+        <h1 className="page-title user-page__title">My list</h1>
+
+        <ul className="user-block">
+          <li className="user-block__item">
+            <div className="user-block__avatar">
+              <img src="img/avatar.jpg" alt="User avatar" width="63" height="63"/>
+            </div>
+          </li>
+          <li className="user-block__item">
+            <a className="user-block__link">Sign out</a>
+          </li>
+        </ul>
+      </header>
+
+      <section className="catalog">
+        <h2 className="catalog__title visually-hidden">Catalog</h2>
+
+        <div className="catalog__films-list">
+          <FilmCard />
+          <FilmCard />
+          <FilmCard />
+          <FilmCard />
+          <FilmCard />
+          <FilmCard />
+          <FilmCard />
+          <FilmCard />
+          <FilmCard />
+        </div>
+      </section>
+
+      <Footer />
+    </div>
+  );
+}
+
+export default MyList;
