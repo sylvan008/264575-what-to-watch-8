a31f7ea4ab6c273e8e4b87c9724451b65c6c3bf9
sylvan008 <sylvan.climb@gmail.com>
2021-10-05 22:21:04 +0300

    Добавит маршрутизатор

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,8 +1,38 @@
-import MainPage from '../main-page/main-page';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import {AppRoute} from '../../utils/const';
 import {PropsType} from './types';
+import AddReview from '../add-review/add-review';
+import MainPage from '../main-page/main-page';
+import MoviePage from '../movie-page/movie-page';
+import MyList from '../my-list/my-list';
+import NotFound from '../not-found/not-found';
+import Player from '../player/player';
 
 function App(props: PropsType): JSX.Element {
-  return <MainPage promo={props.promo} />;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path={AppRoute.Main}>
+          <MainPage promo={props.promo} />;
+        </Route>
+        <Route exact path={AppRoute.AddReview}>
+          <AddReview />
+        </Route>
+        <Route exact path={AppRoute.Film}>
+          <MoviePage />
+        </Route>
+        <Route exact path={AppRoute.MyList}>
+          <MyList />
+        </Route>
+        <Route exact path={AppRoute.Player}>
+          <Player />
+        </Route>
+        <Route>
+          <NotFound />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ project/src/utils/const.ts
@@ -0,0 +1,7 @@
+export enum AppRoute {
+  AddReview = '/films/:id/review',
+  Film = '/films/:id',
+  Main = '/',
+  MyList = '/mylist',
+  Player = '/player/:id'
+}
