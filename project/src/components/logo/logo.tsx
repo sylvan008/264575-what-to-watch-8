df38733f7d6a58a43358aa8944794b80b5f13804
sylvan008 <sylvan.climb@gmail.com>
2021-10-06 17:18:20 +0300

    Добавит Link в компонент Logo

--- project/src/components/logo/logo.tsx
+++ project/src/components/logo/logo.tsx
@@ -1,11 +1,14 @@
+import {Link} from 'react-router-dom';
+import {AppRoute} from '../../utils/const';
+
 function Logo(): JSX.Element {
   return (
     <div className="logo">
-      <a href="main.html" className="logo__link">
+      <Link to={AppRoute.Main} className="logo__link">
         <span className="logo__letter logo__letter--1">W</span>
         <span className="logo__letter logo__letter--2">T</span>
         <span className="logo__letter logo__letter--3">W</span>
-      </a>
+      </Link>
     </div>
   );
 }
