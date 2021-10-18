debf007b58fcecf25a0dbadde994121619d6ca9a
sylvan008 <sylvan.climb@gmail.com>
2021-10-18 12:45:01 +0300

    Создаст управляемую форму для отзывов

--- project/src/components/add-review/add-review.tsx
+++ project/src/components/add-review/add-review.tsx
@@ -1,7 +1,32 @@
+import {ChangeEvent, FormEvent, useState } from 'react';
 import Logo from '../logo/logo';
+import RatingInput from '../rating-input/rating-input';
 import UserBlock from '../user-block/user-block';
 
+const RATING_DEFAULT = '8';
+const RATING_MAX = 10;
+const RATING_MIN = 1;
+
 function AddReview():JSX.Element {
+  const [review, setReview] = useState('');
+  const [rating, setRating] = useState(RATING_DEFAULT);
+
+  function createRatingInputs() {
+    const ratings = [];
+    for (let i = RATING_MAX; i >= RATING_MIN; i--) {
+      ratings.push(<RatingInput ratingValue={`${i}`} changeRating={onRatingChange} key={`rating${i}`} checked={rating === `${i}`} />);
+    }
+    return ratings;
+  }
+
+  function onRatingChange(e: ChangeEvent<HTMLInputElement>) {
+    setRating(e.target.value);
+  }
+
+  function onReviewSubmit(e: FormEvent) {
+    e.preventDefault();
+  }
+
   return (
     <section className="film-card film-card--full">
       <div className="film-card__header">
@@ -34,43 +59,22 @@ function AddReview():JSX.Element {
       </div>
 
       <div className="add-review">
-        <form action="#" className="add-review__form">
+        <form action="#" className="add-review__form" onSubmit={onReviewSubmit}>
           <div className="rating">
             <div className="rating__stars">
-              <input className="rating__input" id="star-10" type="radio" name="rating" value="10"/>
-              <label className="rating__label" htmlFor="star-10">Rating 10</label>
-
-              <input className="rating__input" id="star-9" type="radio" name="rating" value="9"/>
-              <label className="rating__label" htmlFor="star-9">Rating 9</label>
-
-              <input className="rating__input" id="star-8" type="radio" name="rating" value="8" checked/>
-              <label className="rating__label" htmlFor="star-8">Rating 8</label>
-
-              <input className="rating__input" id="star-7" type="radio" name="rating" value="7"/>
-              <label className="rating__label" htmlFor="star-7">Rating 7</label>
-
-              <input className="rating__input" id="star-6" type="radio" name="rating" value="6"/>
-              <label className="rating__label" htmlFor="star-6">Rating 6</label>
-
-              <input className="rating__input" id="star-5" type="radio" name="rating" value="5"/>
-              <label className="rating__label" htmlFor="star-5">Rating 5</label>
-
-              <input className="rating__input" id="star-4" type="radio" name="rating" value="4"/>
-              <label className="rating__label" htmlFor="star-4">Rating 4</label>
-
-              <input className="rating__input" id="star-3" type="radio" name="rating" value="3"/>
-              <label className="rating__label" htmlFor="star-3">Rating 3</label>
-
-              <input className="rating__input" id="star-2" type="radio" name="rating" value="2"/>
-              <label className="rating__label" htmlFor="star-2">Rating 2</label>
-
-              <input className="rating__input" id="star-1" type="radio" name="rating" value="1"/>
-              <label className="rating__label" htmlFor="star-1">Rating 1</label>
+              {createRatingInputs()}
             </div>
           </div>
 
           <div className="add-review__text">
-            <textarea className="add-review__textarea" name="review-text" id="review-text" placeholder="Review text" />
+            <textarea
+              value={review}
+              onChange={(e) => setReview(e.target.value)}
+              className="add-review__textarea"
+              name="review-text"
+              id="review-text"
+              placeholder="Review text"
+            />
             <div className="add-review__submit">
               <button className="add-review__btn" type="submit">Post</button>
             </div>
