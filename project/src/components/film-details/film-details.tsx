39a6eed9c5672c533fd5f2ca45767829c088676e
sylvan008 <sylvan.climb@gmail.com>
2021-10-23 11:48:15 +0300

    Добавит компонент таба с информацией о фильме

---
+++ project/src/components/film-details/film-details.tsx
@@ -0,0 +1,44 @@
+import {Fragment} from 'react';
+import {PropsType} from './props';
+import {humanizeRuntime} from '../../utils/common';
+
+function createActorString(actor: string, index: number, starring: string[]) {
+  return <Fragment key={`${index}${actor}`}>{actor} {(index !== starring.length - 1) && <br />}</Fragment>;
+}
+
+function FilmDetails({film}: PropsType): JSX.Element {
+  const {director, genre, released, runTime, starring} = film;
+  return (
+    <div className="film-card__text film-card__row">
+      <div className="film-card__text-col">
+        <p className="film-card__details-item">
+          <strong className="film-card__details-name">Director</strong>
+          <span className="film-card__details-value">{director}</span>
+        </p>
+        <p className="film-card__details-item">
+          <strong className="film-card__details-name">Starring</strong>
+          <span className="film-card__details-value">
+            {starring.map(createActorString)}
+          </span>
+        </p>
+      </div>
+
+      <div className="film-card__text-col">
+        <p className="film-card__details-item">
+          <strong className="film-card__details-name">Run Time</strong>
+          <span className="film-card__details-value">{humanizeRuntime(runTime)}</span>
+        </p>
+        <p className="film-card__details-item">
+          <strong className="film-card__details-name">Genre</strong>
+          <span className="film-card__details-value">{genre}</span>
+        </p>
+        <p className="film-card__details-item">
+          <strong className="film-card__details-name">Released</strong>
+          <span className="film-card__details-value">{released}</span>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default FilmDetails;
---
+++ project/src/components/film-details/props.ts
@@ -0,0 +1,6 @@
+import {Tab} from '../../types/tab';
+import {Film} from '../../types/film';
+
+export type PropsType = Tab & {
+  film: Film,
+}
