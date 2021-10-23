5316756e333019bf0db6e55fa4059e432617b510
sylvan008 <sylvan.climb@gmail.com>
2021-10-23 11:51:15 +0300

    Добавит компонент таба с общей описанием фильма

---
+++ project/src/components/film-overview/film-overview.tsx
@@ -0,0 +1,28 @@
+import {PropsType} from './props';
+
+function FilmOverview({film}: PropsType): JSX.Element {
+  const {rating, director, starring, description} = film;
+  return (
+    <>
+      <div className="film-rating">
+        <div className="film-rating__score">{rating}</div>
+        <p className="film-rating__meta">
+          <span className="film-rating__level">Very good</span>
+          <span className="film-rating__count">240 ratings</span>
+        </p>
+      </div>
+
+      <div className="film-card__text">
+        <p>{description}</p>
+
+        <p className="film-card__director"><strong>Director: {director}</strong></p>
+
+        <p className="film-card__starring">
+          <strong>Starring: {starring.join(', ')} and other</strong>
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default FilmOverview;
