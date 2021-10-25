e593ef63f35e0015a3d38ccfe4530cd3b7081934
sylvan008 <sylvan.climb@gmail.com>
2021-10-25 11:29:17 +0300

    Подключит редакс в проект

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/app/app';
+import {createStore} from '@reduxjs/toolkit';
+import {Provider} from 'react-redux';
+import {composeWithDevTools} from 'redux-devtools-extension';
 import {filmsMock} from './mocks/film';
 import {reviewsMock} from './mocks/review';
+import {reducer} from './store/reducer';
+import App from './components/app/app';
 
 const promo = {
   name: 'The Grand Budapest Hotel',
@@ -10,12 +14,19 @@ const promo = {
   release: 2014,
 };
 
+const store = createStore(
+  reducer,
+  composeWithDevTools(),
+);
+
 ReactDOM.render(
   <React.StrictMode>
-    <App
-      promo={promo}
-      films={filmsMock}
-      reviews={reviewsMock}
-    />
+    <Provider store={store}>
+      <App
+        promo={promo}
+        films={filmsMock}
+        reviews={reviewsMock}
+      />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'));
