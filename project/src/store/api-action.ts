80c5d683fcbc7ea39226209c8e336e4a3d7bdcbc
sylvan008 <sylvan.climb@gmail.com>
2021-10-28 18:07:37 +0300

    Добавит асинхронные действия

---
+++ project/src/store/api-action.ts
@@ -0,0 +1,46 @@
+import {AuthData} from '../types/auth';
+import {Film} from '../types/film';
+import {ThunkActionResult} from '../types/action';
+import {APIRoute, AuthorizationStatus} from '../utils/const';
+import {requireAuthorization, requireLogout, setFilms} from './action';
+import {dropToken, saveToken, Token} from '../services/token';
+
+/**
+ * Запрос списка фильмов
+ */
+export const fetchFilms = (): ThunkActionResult =>
+  async (dispatch, _getState, api): Promise<void> => {
+    const {data} = await api.get<Film[]>(APIRoute.Films);
+    dispatch(setFilms(data));
+  };
+
+/**
+ * Проверка авторизации
+ */
+export const checkAuthAction = (): ThunkActionResult =>
+  (dispatch, _getState, api): Promise<void> => {
+    return api.get(APIRoute.Login)
+      .then(() => {
+        dispatch(requireAuthorization(AuthorizationStatus.Auth));
+      });
+  };
+
+/**
+ * Вход в аккаунт пользователя
+ */
+export const loginAction = ({email, password}: AuthData): ThunkActionResult =>
+  async (dispatch, _getState, api): Promise<void> => {
+    const {data: {token}} = await api.post<{token: Token}>(APIRoute.Login, {email, password});
+    saveToken(token);
+    dispatch(requireAuthorization(AuthorizationStatus.Auth));
+  };
+
+/**
+ * Выход из аккаунта пользователя
+ */
+export const logoutAction = (): ThunkActionResult =>
+  async (dispatch, _getState, api): Promise<void> => {
+    await api.delete(APIRoute.Logout);
+    dropToken();
+    dispatch(requireLogout());
+  };
--- project/src/types/action.ts
+++ project/src/types/action.ts
@@ -1,4 +1,13 @@
-import {requireAuthorization, requireLogout, setFilms, setGenre, setReviews} from '../store/action';
+import {ThunkAction, ThunkDispatch} from 'redux-thunk';
+import {AxiosInstance} from 'axios';
+import {State} from './state';
+import {
+  requireAuthorization,
+  requireLogout,
+  setFilms,
+  setGenre,
+  setReviews,
+} from '../store/action';
 
 export enum ActionType {
   SetGenre = 'app/set_genre',
@@ -14,3 +23,6 @@ export type Actions =
   | ReturnType<typeof setReviews>
   | ReturnType<typeof requireAuthorization>
   | ReturnType<typeof requireLogout>;
+
+export type ThunkActionResult<R = Promise<void>> = ThunkAction<R, State, AxiosInstance, Actions>;
+export type ThunkAppDispatch = ThunkDispatch<State, AxiosInstance, Actions>
