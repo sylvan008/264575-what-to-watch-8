ad3c8bc85907ec0ccfeb4b6aee87be2991e5b443
sylvan008 <sylvan.climb@gmail.com>
2021-11-02 16:50:07 +0300

    Добавляет логику в компонент userBlock

--- project/src/components/user-block/user-block.tsx
+++ project/src/components/user-block/user-block.tsx
@@ -1,5 +1,36 @@
-function UserBlock(): JSX.Element {
-  return (
+import {Dispatch} from 'react';
+import {connect, ConnectedProps} from 'react-redux';
+import {Link} from 'react-router-dom';
+import {AppRoute, AuthorizationStatus} from '../../utils/const';
+import {State} from '../../types/state';
+import {Actions, ThunkAppDispatch} from '../../types/action';
+import {logoutAction} from '../../store/api-action';
+
+function mapStateToProps({authorizationStatus}: State) {
+  return {
+    authorizationStatus,
+  };
+}
+
+function mapDispatchToProps(dispatch: Dispatch<Actions>) {
+  return {
+    onLogoutClick() {
+      (dispatch as ThunkAppDispatch)(logoutAction());
+    },
+  };
+}
+
+const connector = connect(mapStateToProps, mapDispatchToProps);
+type PropsFromRedux = ConnectedProps<typeof connector>;
+
+/**
+ * Компонент предоставляет разметку в зависимости от статуса пользователя:
+ * - не авторизован, ссылка на страницу авторизации;
+ * - авторизован, автар и кнопка выхода из аккаунта.
+ */
+function UserBlock({authorizationStatus, onLogoutClick}: PropsFromRedux): JSX.Element {
+  const isAuth = authorizationStatus === AuthorizationStatus.Auth;
+  const userBlock = (
     <ul className="user-block">
       <li className="user-block__item">
         <div className="user-block__avatar">
@@ -7,10 +38,22 @@ function UserBlock(): JSX.Element {
         </div>
       </li>
       <li className="user-block__item">
-        <a href="#" className="user-block__link">Sign out</a>
+        <a href="#" className="user-block__link" onClick={(e) => {
+          e.preventDefault();
+          onLogoutClick();}}
+        >
+          Sign out
+        </a>
       </li>
     </ul>
   );
+  const loginBLock = (
+    <div className="user-block">
+      <Link to={AppRoute.Login} className="user-block__link">Sign in</Link>
+    </div>
+  );
+  return isAuth ? userBlock : loginBLock;
 }
 
-export default UserBlock;
+export {UserBlock};
+export default connector(UserBlock);
