52507de98ea9b941e45ed06a39cacf0ab0e1eb79
sylvan008 <sylvan.climb@gmail.com>
2021-11-20 21:46:03 +0300

    Добавляет функцию переводящее оставшееся время длительности фильма, в удобный для пользователя формат

--- project/src/utils/common.ts
+++ project/src/utils/common.ts
@@ -3,6 +3,9 @@ import {NormalizedRating} from '../types/review';
 import {RouteParamsValues} from '../types/url-params';
 
 const HOUR = 60;
+const MINUTE = 60;
+
+const HOUR_SECONDS = 3600;
 
 /**
  * Делает время продолжительности фильма, удобочитаемым для людей
@@ -11,6 +14,37 @@ function humanizeRuntime(duration: number): string {
   const hours = Math.floor(duration / HOUR);
   const minutes = duration % HOUR;
   return `${hours}h ${minutes}m`;
+}
+
+
+/**
+ *  Делает оставшееся время фильма, удобочитаемым для людей
+ */
+function humanizeVideoTimeLeft(time = 0): string {
+  if  (!time) {
+    return '0:00:00';
+  }
+  const durationMinutes = time % HOUR_SECONDS;
+  const durationSeconds = durationMinutes % MINUTE;
+
+  let hours = Math.floor(time / HOUR_SECONDS),
+    minutes = Math.floor(durationMinutes / MINUTE),
+    seconds = Math.ceil(durationSeconds);
+
+  if (seconds === MINUTE) {
+    seconds = 0;
+    minutes = minutes + 1;
+  }
+  if (minutes === HOUR) {
+    minutes = 0;
+    hours = hours + 1;
+  }
+  const mm = `${minutes < 10 ? '0' : ''}${minutes}`;
+  const ss = `${seconds < 10 ? '0' : ''}${seconds}`;
+
+  return hours === 0
+    ? `-${mm}:${ss}`
+    : `-${hours}:${mm}:${ss}`;
 }
 
 /**
@@ -44,6 +78,7 @@ function replaceRouteParams(route: APIRoute | AppRoute, param: RouteParamsValues
 export {
   classNames,
   humanizeRuntime,
+  humanizeVideoTimeLeft,
   humanizedRating,
   replaceRouteParams
 };
